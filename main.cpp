603ce97b1225e961b9a24789fe5838b8910cafb7
ilianzapryanov <ilian.zapryanov@balkantel.net>
2016-06-07 13:56:54 +0300

    main: added tests to the main !

--- main.cpp
+++ main.cpp
@@ -10,15 +10,30 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    SignalRPC* srpc = new SignalRPC("192.168.32.204",
+                                    4000,
+                                    "",
+                                    "");
+
+    srpc->init();
+
+    ptt p1("KA");
+    ptt p2("FV");
+    ptt p3("PN");
+    ptt p4("PF");
 
 
+    p1.registerRpc(srpc);
+    p2.registerRpc(srpc);
+    p3.registerRpc(srpc);
+    p4.registerRpc(srpc);
 
-    // create a test ami clients //
-    //ptt p1("ptt 1");
-    ptt p1("ptt 1");
-    ptt p2("ptt 2");
-    ptt p3("ptt 3");
-    ptt p4("ptt 4");
+
+    p1.setCommand("ka\n");
+    p2.setCommand("fv\n");
+    p3.setCommand("pn\n");
+    p4.setCommand("pf\n");
+
 
     // get a handle to the ami //
 
